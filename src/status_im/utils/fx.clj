765839fafe6774aec935ab677ac62a1eb633d2fe
yenda <eric@status.im>
2019-05-23 17:30:47 +0200

    wip

--- src/status_im/utils/fx.clj
+++ src/status_im/utils/fx.clj
@@ -1,7 +1,7 @@
 (ns status-im.utils.fx
   (:refer-clojure :exclude [defn]))
 
-(defmacro defn
+(defmacro defn2
   "Defines an fx producing function
   Takes the same arguments as the defn macro
   Produces a 2 arity function:
@@ -40,3 +40,54 @@
           (let [~cofx cofx#]
             ~@fdecl)
           (throw (js/Error. (str "fx/defn expects a map of cofx as first argument got " cofx# " in function " ~name))))))))
+
+(defmacro defn
+  "Defines an fx producing function
+  Takes the same arguments as the defn macro
+  Produces a 2 arity function:
+  - first arity takes the declared parameters and returns a function that takes cofx as
+  single argument, for use in composition of effects
+  - second arity takes cofx as first arguments and declared parameters as next arguments,
+  for use in repl or direct call
+  Notes:
+  - destructuring of cofx is possible
+  - supports docstring
+  - supports attr-map
+  - TODO: add suport for `prepost-map?` (don't forget to add it to arglist)
+  - TODO: add validation of macro parameters"
+  {:arglists '([name doc-string? attr-map? [params*] body])}
+  [name & fdecl]
+  (let [m (if (string? (first fdecl))
+            {:doc (first fdecl)}
+            {})
+        fdecl (if (string? (first fdecl))
+                (next fdecl)
+                fdecl)
+        m (if (map? (first fdecl))
+            (conj m (first fdecl))
+            m)
+        events (get m :events [])
+        fdecl (if (map? (first fdecl))
+                (next fdecl)
+                fdecl)
+        [cofx & args] (first fdecl)
+        fdecl (next fdecl)
+        argsyms (take (count args) (repeatedly #(gensym "arg")))]
+    (if (and (sequential? events)
+             (every? keyword? events))
+      `(let [event-fn# (fn [cofx# ~@args]
+                         (if (and (map? cofx#)
+                                  (not (nil? (:db cofx#))))
+                           (let [~cofx cofx#]
+                             ~@fdecl)
+                           (throw (js/Error. (str "fx/defn expects a map of cofx as first argument got "
+                                                  cofx# "in function " ~(clojure.core/name name))))))]
+         (clojure.core/defn ~(with-meta name m)
+           ([~@args] (fn [cofx#] (event-fn# cofx# ~@args)))
+           ([cofx# ~@args] (event-fn# cofx# ~@args)))
+         ~@(map (fn [event]
+                  `(status-im.utils.handlers/register-handler-fx
+                    ~event
+                    (fn [cofx# [_ ~@args]] (event-fn# cofx# ~@args))))
+                events))
+      (throw (Exception. (str "fx/defn expects a vector of keyword as value for :events key in attr-map in function " name))))))
